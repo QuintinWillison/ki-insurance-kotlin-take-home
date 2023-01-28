954110a301c1d0941a546619620bc51995a592c7
Quintin Willison <q@qwuk.net>
2023-01-28 14:34:46 +0000

    Add KDoc commentary to primary APIs which must not be altered.

--- src/main/kotlin/com/ki/services/PaymentProcessor.kt
+++ src/main/kotlin/com/ki/services/PaymentProcessor.kt
@@ -5,7 +5,20 @@ import com.opencsv.CSVReaderBuilder
 import java.io.FileReader
 import java.io.IOException
 
+/**
+ * Service class offering methods for loading and processing payment records.
+ *
+ * **IMPORTANT**: The API offered by this class is relied upon by other services.
+ * The module it's included in is installed on the platform as a dependency for those other services.
+ */
 class PaymentProcessor {
+    /**
+     * Read payment records from the supplied CSV file.
+     *
+     * @param csvPath Path to the payments CSV file.
+     * @param source The source of the payments, currently ignored.
+     * @return Payment records, in the order read from the file.
+     */
     fun getPayments(
         csvPath: String,
         @Suppress("UNUSED_PARAMETER") source: String,
@@ -25,6 +38,12 @@ class PaymentProcessor {
         return payments.toArray(arrayOf())
     }
 
+    /**
+     * Filter payment records, removing those which are invalid for some reason (e.g. declined).
+     *
+     * @param payments The payment records to filter.
+     * @return Valid payment records. This array could contain from zero to [payments].`size` records.
+     */
     fun verifyPayments(payments: Array<Payment>): Array<Payment> {
         val filtered = ArrayList<Payment>()
         for (payment in payments) {
